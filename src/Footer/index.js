aab9aa9430ee71661f07c3111460588a847cc0ae
Carolina Beirigo <cs16572@gmail.com>
2021-08-06 00:53:17 -0300

    creation of all components

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,9 +1,57 @@
 import React from 'react';
+import './styles.scss';
+import instagramIcon from '../assets/img/svg/instagram-icon.svg';
+import facebookIcon from '../assets/img/svg/facebook-icon.svg';
+import linkedinIcon from '../assets/img/svg/linkedin-icon.svg';
+import locationIcon from  '../assets/img/svg/location-icon.svg';
+import whatsappIcon from  '../assets/img/svg/whatsapp-icon.svg';
+import emailIcon from  '../assets/img/svg/email-icon.svg';
 
-// import { Container } from './styles';
+
+
 
 function Footer() {
-  return <div />;
+  return (
+    <section className="footer">
+      <div className="footer__container">
+        <div className="footer__content">
+          <div>
+            <a className="footer__logo">mapear</a>
+          </div>
+
+          <div className="footer__social-media">
+            <a href="#"> <img src={instagramIcon}></img></a>
+            <a href="#"><img src={linkedinIcon}></img></a>
+            <a href="#"> <img src={facebookIcon}></img></a>
+          </div>
+
+          <div className="footer__contact">
+            <div className="footer__address">
+              <a href="#">
+                <img src={locationIcon}></img>
+              </a>
+              <p>Av. Afonso Pena, 4465 – Umuarama
+                Uberlândia – MG, CEP: 38405-309</p>
+            </div>
+
+            <div className="footer__whatsapp-number">
+              <a href="#">
+                <img src={whatsappIcon}></img>
+              </a>
+              <p>(34) 98431-1111</p>
+            </div>
+
+            <div className="footer__email">
+              <a href="#">
+                <img src={emailIcon}></img>
+              </a>
+              <p>fale@mapearperfil.com.br</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default Footer;
--- src/FrequentlyAskedQuestions/index.js
+++ src/FrequentlyAskedQuestions/index.js
@@ -1,13 +1,39 @@
 import React from 'react';
 import './styles.scss';
+import arrowQuestions from '../assets/img/svg/arrow-questions.svg';
 
 function FrequentlyAskedQuestions() {
-    return(
-        <section className="requently-asked-questions">
-            <div className="requently-asked-questions__content-title">
-                    <h2 className="requently-asked-questions__title title">
-                        Dúvidas Frequentes
-                    </h2>
+    return (
+        <section className="frequently-asked-questions">
+            <div className="frequently-asked-questions__content-title">
+                <h2 className="frequently-asked-questions__title title">
+                    Dúvidas Frequentes
+                </h2>
+            </div>
+
+            <article className="frequently-asked-questions__content">
+                <div className="frequently-asked-questions__question">
+                    <p>Preciso de alguma graduação para fazer a Formação?</p>
+                    <div>
+                        <img src={arrowQuestions}></img>
+                    </div>
+                </div>
+                <div className="frequently-asked-questions__question">
+                    <p>A Formação é online ou presencial?</p>
+                    <div>
+                        <img src={arrowQuestions}></img>
+                    </div>
+                </div>
+                <div className="frequently-asked-questions__question">
+                    <p>Qual o valor da Formação?</p>
+                    <div>
+                        <img src={arrowQuestions}></img>
+                    </div>
+                </div>
+            </article>
+
+            <div className="frequently-asked-questions__button-contact-us">
+                    <a href="#" className="btn">Inscrever</a>
                 </div>
         </section>
     )

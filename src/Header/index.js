4c62e55aba7837effb8dbeb6b60a30193c081408
Carolina Beirigo <cs16572@gmail.com>
2021-08-07 00:47:03 -0300

    media query creation max-width resolution 770px

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './style.scss';
 
+
 function Header() {
   return (
     <header className="header">
       <div className="container">
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-light ">
           <div className="container-fluid">
             <a className="navbar-brand" href="#">mapear</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -14,7 +15,7 @@ function Header() {
             <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
               <ul className="navbar-nav">
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">O que é a formação?</a>
+                  <a className="nav-link active" aria-current="page" href="#">O que é a Formação?</a>
                 </li>
                 <li className="nav-item">
                   <a className="nav-link" href="#">Benefícios</a>

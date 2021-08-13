4da1aa4f3adb17c61187026d014b9f0afe36e6dc
gabeirigo <gabriel.beirigoweb@gmail.com>
2021-08-13 12:37:39 -0300

    Update

--- src/Header/index.js
+++ src/Header/index.js
@@ -9,7 +9,7 @@ function Header() {
         <nav className="navbar navbar-expand-lg navbar-light ">
           <div className="container-fluid">
             <a className="navbar-brand" href="#">
-              <img src={mapearBrand}></img>
+              <img src={mapearBrand} alt="Logo"></img>
             </a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>

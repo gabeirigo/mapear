1353ea7eb1045e50c55bdbcc6d8104ac01721c9a
gabeirigo <gabriel.beirigoweb@gmail.com>
2021-10-13 10:45:49 -0300

    Update wpp contact

--- src/Info/index.js
+++ src/Info/index.js
@@ -12,7 +12,7 @@ function Info() {
             <div className="info__professional-career">
               <h2 className="info__title title">Dê um grande passo na sua carreira profissional</h2>
               <p className="info__description">Certificamos você no uso da ferramentas de Avaliação de Perfil Comportamental  para atuar de forma mais assertiva e segura em diversas áreas, Treinamento, Desenvolvimento,
-                coaching, Recursos Humanos,  Recrutamento e Seleção,   Retenção de Talentos, Aumento de Produtividade, Formação de Liderança,  Gestão de Performance,  Gestão de Competências. <a href="">Entre em contato para saber mais sobre próxima turma. </a> </p>
+                coaching, Recursos Humanos,  Recrutamento e Seleção,   Retenção de Talentos, Aumento de Produtividade, Formação de Liderança,  Gestão de Performance,  Gestão de Competências. <a href="https://api.whatsapp.com/send?phone=5534984311111">Entre em contato para saber mais sobre próxima turma. </a> </p>
             </div>
             <div className="info__images-rhImage">
               <img className="info__image" src={rhImage} alt="Formação Analista Comportamental" title="Formação Analista Comportamental" />
